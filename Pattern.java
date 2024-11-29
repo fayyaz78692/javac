cfb17273bf52a9d0b089b04786c6a68237055aeb
fayyaz78692 <fayyazmansoori256@gmail.com>
2024-11-29 15:15:17 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,124 @@
+class pattern{
+    public static void main(String[] args) {
+        for(int i=5;i<=7;i++){
+            for(int j=5;j<=9;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=5;i<=7;i++){
+            for(int j=8;j>=4;j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=1;i<=4;i++){
+            for(char j='A';j<='E';j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=1;i<=6;i++){
+            for(char j='e';j>='a';j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=1;i<=4;i++){
+            for(int j=2;j<=4;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(char i='A';i<='E';i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(char i='A';i<='E';i++){
+            for(char j='A';j<=i;j++){
+                System.out.print("A");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=1;i<=4;i++){
+            for(int j=5;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i='E';i>='A';i--){
+            for(char j='A';j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for(int i=5;i>=1;i--){
+            for(int j=i;j>1;j--){
+                System.out.print(" ");
+            }
+            for(int k=i;k<=5;k++){
+                System.out.print(k);
+            }
+            System.out.println(" ");
+        }
+        System.out.println("\n");
+        for(int i=1;i<=5;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(" "+k);
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+        int k;
+        for(int i=1;i<=5;i++){
+            for(int j=i;j<5;j++){
+                System.out.print(" ");
+            }
+            for( k=1;k<=i;k++){
+                System.out.print(k);
+            }
+            for(int x=k-2;x>=1;x--){
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+  
+    
+        
+    }
+}
